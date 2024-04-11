991e697d8d6c292344cc7282bee783db7edaefd5
Md Shakib Hossain <shakibhossainmuhammad@gmail.com>
2024-04-11 09:38:47 +0600

    initial commit

---
+++ components/SearchBar.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import { FormEvent, useState } from "react";
+import { SearchManuFacturer } from "./";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
+  <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
+    <Image
+      src={"/magnifying-glass.svg"}
+      alt={"magnifying glass"}
+      width={40}
+      height={40}
+      className="object-contain"
+    />
+  </button>
+);
+
+const SearchBar = ({setManufacturer, setModel}) => {
+  const [searchManufacturer, setSearchManufacturer] = useState("");
+  const [searchModel, setSearchModel] = useState("");
+  const router = useRouter();
+
+  const handleSearch = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (searchManufacturer === "" && searchModel === "") {
+      return alert("Please fill in ther search bar");
+    }
+
+    setModel(searchModel) 
+    setManufacturer(searchManufacturer)
+  };
+
+  
+
+  return (
+    <form className="searchbar" onSubmit={handleSearch}>
+      <div className="searchbar__item">
+        <SearchManuFacturer
+          selected={searchManufacturer}
+          setSelected={setSearchManufacturer}
+        />
+
+        <SearchButton otherClasses="sm:hidden" />
+      </div>
+      <div className="searchbar__item">
+        <Image
+          src="/model-icon.png"
+          width={25}
+          height={25}
+          className="absolute w-[20px] h-[20px] ml-4"
+          alt="car model"
+        />
+        <input
+          type="text"
+          name="model"
+          value={searchModel}
+          onChange={(e) => setSearchModel(e.target.value)}
+          placeholder="Tiguan"
+          className="searchbar__input"
+        />
+        <SearchButton otherClasses="sm:hidden" />
+      </div>
+      <SearchButton otherClasses="max-sm:hidden" />
+    </form>
+  );
+};
+
+export default SearchBar;
